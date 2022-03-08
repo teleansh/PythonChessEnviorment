80be17a453b3430f9f6087a7edc5fac54bf7f7f8
teleansh <34571056+teleansh@users.noreply.github.com>
2022-03-08 13:10:03 +0530

    Add files via upload

--- PythonChessEnviroment.py
+++ PythonChessEnviroment.py
@@ -5,14 +5,26 @@ def newBoard():
     
     b = "r n b q k b n r p p p p p p p p".split(" ") + ['.']*32 + "p p p p p p p p r n b q k b n r".upper().split(" ")
         
-def display(): #black side view 
-    c= 1
-    for i in b[::-1]:
-        print(i,end='')
+def display(): #white side view 
+    c , k= 1 ,0
+    ap = range(1,9)[::-1]
+    row,col=[],[]
+    for i in b:
+        row.append(i)
         if c==8 : 
             c=0
-            print()
+            col.append(row)
+            row=[]
         c+=1
+
+    for j in col[::-1]:
+        print(ap[k] , " |" ,end=" ")
+        for i in j:
+            print(i,end=' ')
+        print()
+        k+=1
+    print("   ",end="")
+    print("-"*18,"     A B C D E F G H",sep="\n")
 
 
 def move(fr,to):
@@ -156,5 +168,343 @@ def addressValid(fnum,tnum):
     return 0<=fnum<64 and 0<=tnum<64
 
 def rookMoves(pos):
-    #code goes here
-    print()
+    num=(conv(pos))-1                   #num is index
+    if b[num].isupper() : c='b'
+    elif b[num].islower() : c='w'
+    else: return "Block is empty"
+    vm=[]
+    col=(num+1)%8
+    if col==0: col=8
+    
+    row=int(pos[1])
+    
+    if c=='w':
+        block=num+8
+        while row<=8:
+            if b[block] == '.' : vm.append(block)
+            if b[block].isupper() :
+                vm.append(block)
+                break
+            if b[block].islower():
+                break
+            block+=8
+            row+=1
+
+        row=int(pos[1])
+
+        
+        block=num-8
+        while row>0:
+            if b[block] == '.' : vm.append(block)
+            if b[block].isupper() :
+                vm.append(block)
+                break
+            if b[block].islower():
+                break
+            block-=8
+            row-=1
+
+        tcol=col+1                      #col is from 1 to 8 , row is from 1 to 8
+       
+ 
+        block =num+1
+        while tcol<=8:
+            if b[block] == '.' : vm.append(block)
+            if b[block].isupper() :
+                vm.append(block)
+                break
+            if b[block].islower():
+                break
+            
+            block+=1
+            tcol+=1
+
+        block =num-1
+        tcol=col
+        while tcol>1:
+            if b[block] == '.' : vm.append(block)
+            if b[block].isupper() :
+                vm.append(block)
+                break
+            if b[block].islower():
+                break
+            block-=1
+            tcol-=1
+
+        tcol=col
+        row=int(pos[1])
+        
+
+    if c=='b':
+        block=num+8
+        while row<=8:
+            if b[block] == '.' : vm.append(block)
+            if b[block].islower() :
+                vm.append(block)
+                break
+            if b[block].isupper():
+                break
+            block+=8
+            row+=1
+
+        row=int(pos[1])
+
+        
+        block=num-8
+        while row>1:
+            if b[block] == '.' : vm.append(block)
+            if b[block].islower() :
+                vm.append(block)
+                break
+            if b[block].isupper():
+                break
+            block-=8
+            row-=1
+
+        tcol=col+1                      #col is from 1 to 8 , row is from 1 to 8
+       
+ 
+        block =num+1
+        while tcol<=8:
+            if b[block] == '.' : vm.append(block)
+            if b[block].islower() :
+                vm.append(block)
+                break
+            if b[block].isupper():
+                break
+            
+            block+=1
+            tcol+=1
+
+        block =num-1
+        tcol=col
+        while tcol>1:
+            if b[block] == '.' : vm.append(block)
+            if b[block].islower() :
+                vm.append(block)
+                break
+            if b[block].isupper():
+                break
+            block-=1
+            tcol-=1
+
+    
+    move=[]
+    for l in vm:
+        move.append(numToAlg(l))
+
+    return move
+
+def bishopMoves(pos):
+    num=(conv(pos))-1
+    if b[num].isupper() : c='b'
+    elif b[num].islower() : c='w'
+    else: return "Block is empty"
+    vm=[]
+    col=(num+1)%8
+    if col==0: col=8
+    
+    row=int(pos[1])+1
+    
+    if c=='w':
+
+        tcol=col+1
+        row=int(pos[1])+1
+        block=num+9
+        
+        while row<=8 and col<=8 :                  #goes top right
+            if b[block] == '.' : vm.append(block)
+            if b[block].isupper() :
+                vm.append(block)
+                break
+            if b[block].islower():
+                break
+            block+=9
+            row+=1
+            tcol+=1
+
+        row=int(pos[1])-1
+        tcol=col-1
+        
+        block=num-9
+        
+        while row>0 and tcol>1:                 #goes below left
+            if b[block] == '.' : vm.append(block)
+            if b[block].isupper() :
+                vm.append(block)
+                break
+            if b[block].islower():
+                break
+            block-=9
+            row-=1
+            tcol-=1
+
+        row=int(pos[1])-1
+        tcol=col+1                      
+       
+ 
+        block =num-7
+        while tcol<=8 and row>1:                              #goes below right
+            if b[block] == '.' : vm.append(block)
+            if b[block].isupper() :
+                vm.append(block)
+                break
+            if b[block].islower():
+                break
+            
+            block-=7
+            tcol+=1
+            row-=1
+
+        block =num+7
+        tcol=col-1
+        row=int(pos[1])+1
+
+        while tcol>0 and row<=8:                                   #goes top left
+            if b[block] == '.' : vm.append(block)
+            if b[block].isupper() :
+                vm.append(block)
+                break
+            if b[block].islower():
+                break
+            block+=7
+            tcol-=1
+            row+=1
+
+
+    if c=='b':
+
+        tcol=col+1
+        row=int(pos[1])+1
+        block=num+9
+        
+        while row<=8 and col<=8 :                  #goes top right
+            if b[block] == '.' : vm.append(block)
+            if b[block].islower() :
+                vm.append(block)
+                break
+            if b[block].isupper():
+                break
+            block+=9
+            row+=1
+            tcol+=1
+
+        row=int(pos[1])-1
+        tcol=col-1
+        
+        block=num-9
+        
+        while row>0 and tcol>1:                 #goes below left
+            if b[block] == '.' : vm.append(block)
+            if b[block].islower() :
+                vm.append(block)
+                break
+            if b[block].isupper():
+                break
+            block-=9
+            row-=1
+            tcol-=1
+
+        row=int(pos[1])-1
+        tcol=col+1                      
+       
+ 
+        block =num-7
+        while tcol<=8 and row>1:                              #goes below right
+            if b[block] == '.' : vm.append(block)
+            if b[block].islower() :
+                vm.append(block)
+                break
+            if b[block].isupper():
+                break
+            
+            block-=7
+            tcol+=1
+            row-=1
+
+        block =num+7
+        tcol=col-1
+        row=int(pos[1])+1
+
+        while tcol>0 and row<=8:                                   #goes top left
+            if b[block] == '.' : vm.append(block)
+            if b[block].islower() :
+                vm.append(block)
+                break
+            if b[block].upper():
+                break
+            block+=7
+            tcol-=1
+            row+=1
+        
+
+    
+
+    
+    move=[]
+    for l in vm:
+        move.append(numToAlg(l))
+
+    return move
+
+
+def queenMoves(pos):
+    return rookMoves(pos) + bishopMoves(pos)
+
+
+def knightMoves(pos):
+    num = conv(pos)-1   #num is index
+    vm = [num-17,num-15,num-10,num-6,num+6,num+10,num+15,num+17]
+
+    if vm[3]%8 in [0,1]:
+        vm.pop(3)
+        vm.pop(5)
+    if vm[4]%8 in [6,7]:
+        vm.pop(4)
+        vm.pop(2)
+    
+    tvm=[]
+    
+    for i in vm:
+        if (i>=0 and i<=63) and not ((b[num].isupper and b[i].isupper()) or (b[num].islower and b[i].islower())) : tvm.append(i)
+    move=[]
+    for l in tvm:
+        move.append(numToAlg(l))
+    return move
+
+def kingMoves(pos):
+    num = conv(pos)-1   #num is index
+    vm = [num+8,num-8,num+9,num-9,num+7,num-7,num+1,num-1]
+
+    if vm[2]%8==0:
+        vm.pop(2)
+        vm.pop(6)
+        vm.pop(5)
+    if vm[3]%8 ==7:
+        vm.pop(3)
+        vm.pop(-1)
+        vm.pop(4)
+    
+    tvm=[]
+    
+    for i in vm:
+        if (i>=0 and i<=63) and not ((b[num].isupper and b[i].isupper()) or (b[num].islower and b[i].islower())) : tvm.append(i)
+    move=[]
+    for l in tvm:
+        move.append(numToAlg(l))
+    return move
+
+def pawnMoves(pos):
+    #to be worked upon
+    return True
+
+
+        
+def numToAlg(ind):
+    alp=(ind+1)%8
+    n=((ind+1)//8) + 1
+    if alp==0:
+        n-=1
+    a = {0:'h',1 : 'a' , 2:'b',3:'c',4:'d',5:'e',6:'f',7:'g',8:'h'}
+    return str(a[alp]) + str(n)
+    
